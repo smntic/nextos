4ff7a11ae5d8616ae5c60ee4c1cd7148f80b3320
Simon Ashton <simonashton.dev@gmail.com>
2024-12-10 18:54:57 +0000

    :Fix install script with disko

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
-# Partition and format disk
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko.nix
+
+# Check if hostname argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: ./install.sh HOSTNAME"
+    exit 1
+fi
+
+# Set hostname and disk configuration path
+HOSTNAME=$1
+DISKO_PATH="./hosts/$HOSTNAME/disko.nix"
+
+# Check if the disko configuration file exists for the given hostname
+if [ ! -f "$DISKO_PATH" ]; then
+    echo "Error: Disk configuration file $DISKO_PATH not found!"
+    exit 1
+fi
+
+# Partition and format disk using the specific disko configuration
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko "$DISKO_PATH"
 if [ $? -ne 0 ]; then
     echo "Error in partitioning or formatting disk!"
     exit 1
@@ -28,3 +45,4 @@ echo "Done installing NixOS!"
 
 # Welcome the user
 echo "Welcome to nextOS!"
+
