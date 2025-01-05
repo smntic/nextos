52f6c956d028c57acafe21be653848e451e73871
Simon Ashton <simonashton.dev@gmail.com>
2025-01-04 19:23:56 -0800

    Fix precomputed header files

--- home_modules/precomp_bits.nix
+++ home_modules/precomp_bits.nix
@@ -1,20 +1,14 @@
 { pkgs, lib, config, ... }:
 
 let
-  precompBits = pkgs.stdenv.mkDerivation {
-    pname = "precomp-bits";
-    version = "1.0";
-
-    # Skip unpack phase as there is no source to unpack
-    dontUnpack = true;
-
-    installPhase = ''
-      mkdir -p $out
-      export BITS_PATH=${pkgs.gcc.cc}/include/c++/${pkgs.gcc.version}/$(gcc -dumpmachine)/bits/
-      cp $BITS_PATH/stdc++.h $out/
-      ${pkgs.gcc}/bin/g++ -x c++-header $out/stdc++.h -o $out/stdc++.h.gch
-    '';
-  };
+  precompBits = pkgs.runCommand "precomp-bits" {
+    buildInputs = [ pkgs.gcc ];
+  } ''
+    mkdir -p $out
+    export BITS_PATH=${pkgs.gcc.cc}/include/c++/${pkgs.gcc.version}/$(gcc -dumpmachine)/bits/
+    cp $BITS_PATH/stdc++.h $out/
+    ${pkgs.gcc}/bin/g++ -x c++-header $out/stdc++.h -o $out/stdc++.h.gch
+  '';
 in
   {
     options = {
@@ -26,7 +20,6 @@ in
     };
     
     config = lib.mkIf config.homeModules.precomp-bits.enable {
-      home.packages = [ precompBits ];
-      homeModules.precomp-bits.out = builtins.toString precompBits.out;
+      homeModules.precomp-bits.out = builtins.toString precompBits;
     };
   }
