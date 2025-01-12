4e85bead6766ef99bf392df3d6538bacbb8f4a66
Simon Ashton <simonashton.dev@gmail.com>
2025-01-11 17:23:34 -0800

    Remove colemak and keyboard layers
    ...for now, i admit defeat.

--- hosts/x13/kmonad.nix
+++ hosts/x13/kmonad.nix
@@ -25,50 +25,59 @@
             lctl lmet lalt             spc              ralt sys  rctl
           )
 
-          (defalias rnm (layer-toggle reach-no-more))
-          (defalias srnm (layer-toggle shift-reach-no-more))
-          (defalias num (layer-toggle numpad))
-
-          (deflayer colemak-dh
-            grv   1    2    3    4    5    6    7    8    9    0    -    =     bspc
-            tab    q    w    f    p    b    j    l    u    y    ;    bspc del  @num
-            esc     a    r    s    t    g    m    n    e    i    o    @rnm     ret
-            lsft     x    c    d    v    z    k    h    ,    .    rsft         rsft
-            lctl lalt lmet             spc              rmet ralt rctl
-          )
-
           (deflayer qwerty
            grv   1    2    3    4    5    6    7    8    9    0    -    =     bspc
-           tab    q    w    e    r    t    y    u    i    o    p    bspc del  @num
-           esc     a    s    d    f    g    h    j    k    l    ;    @rnm     ret
-           lsft     z    x    c    v    b    n    m    ,    .    rsft         rsft
+           tab    q    w    e    r    t    y    u    i    o    p    [    ]    \
+           esc     a    s    d    f    g    h    j    k    l    ;    '        ret
+           lsft     z    x    c    v    b    n    m    ,    .     /           rsft
            lctl lalt lmet             spc              rmet ralt rctl
           )
-
-          (deflayer reach-no-more
-            _     _    _    <    >    _    _    _    _    _    _    _    _     _
-            _      -    +    {    }    |    down rght ?    !    _    _    _    _
-            _       /    *    \(   \)   &    \    "    '    `    _    _        _
-            @srnm    =    ^    [    ]    _    up   left #    @    @srnm        @srnm
-            _    _    _                \_               _    sys  _   
-          )
-
-          (deflayer shift-reach-no-more
-            _     _    _    _    _    _    _    _    _    _    _    _    _     _
-            _      _    _    _    _    _    pgdn end  _    _    _    _    _    _
-            _       _    _    _    _    _    _    _    _    ~    _    _        _
-            _        _    _    _    _    _    pgup home $    %    _            _
-            _    _    _                _                _    _    _   
-          )
-
-          (deflayer numpad
-            _     _    7    8    9    bspc _    _    _    _    _    _    _     _
-            _      ^    4    5    6    -    _    _    _    _    _    _    _    _
-            _       /    1    2    3    +    _    _    _    _    _    _        _
-            _        *    .    0    =    ret  _    _    _    _    _            _
-            _    _    _                _                _    _    _   
-          )
         '';
+
+          # (defalias rnm (layer-toggle reach-no-more))
+          # (defalias srnm (layer-toggle shift-reach-no-more))
+          # (defalias num (layer-toggle numpad))
+          #
+          # (deflayer colemak-dh
+          #   grv   1    2    3    4    5    6    7    8    9    0    -    =     bspc
+          #   tab    q    w    f    p    b    j    l    u    y    ;    bspc del  @num
+          #   esc     a    r    s    t    g    m    n    e    i    o    @rnm     ret
+          #   lsft     x    c    d    v    z    k    h    ,    .    rsft         rsft
+          #   lctl lalt lmet             spc              rmet ralt rctl
+          # )
+          #
+          # (deflayer qwerty
+          #  grv   1    2    3    4    5    6    7    8    9    0    -    =     bspc
+          #  tab    q    w    e    r    t    y    u    i    o    p    bspc del  @num
+          #  esc     a    s    d    f    g    h    j    k    l    ;    @rnm     ret
+          #  lsft     z    x    c    v    b    n    m    ,    .    rsft         rsft
+          #  lctl lalt lmet             spc              rmet ralt rctl
+          # )
+          #
+          # (deflayer reach-no-more
+          #   _     _    _    <    >    _    _    _    _    _    _    _    _     _
+          #   _      -    +    {    }    |    down rght ?    !    _    _    _    _
+          #   _       /    *    \(   \)   &    \    "    '    `    _    _        _
+          #   @srnm    =    ^    [    ]    _    up   left #    @    @srnm        @srnm
+          #   _    _    _                \_               _    sys  _   
+          # )
+          #
+          # (deflayer shift-reach-no-more
+          #   _     _    _    _    _    _    _    _    _    _    _    _    _     _
+          #   _      _    _    _    _    _    pgdn end  _    _    _    _    _    _
+          #   _       _    _    _    _    _    _    _    _    ~    _    _        _
+          #   _        _    _    _    _    _    pgup home $    %    _            _
+          #   _    _    _                _                _    _    _   
+          # )
+          #
+          # (deflayer numpad
+          #   _     _    7    8    9    bspc _    _    _    _    _    _    _     _
+          #   _      ^    4    5    6    -    _    _    _    _    _    _    _    _
+          #   _       /    1    2    3    +    _    _    _    _    _    _        _
+          #   _        *    .    0    =    ret  _    _    _    _    _            _
+          #   _    _    _                _                _    _    _   
+          # )
+        # '';
       };
     };
   };
