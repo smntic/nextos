f309f8cad36ee964c47422eaee7a18f304ddbb3c
Simon Ashton <simonashton.dev@gmail.com>
2025-01-26 13:05:33 -0800

    Remove nvim indent-blankline

--- home_modules/nvim/plugins/blankline.nix
+++ home_modules/nvim/plugins/blankline.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
 {
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-    indent-blankline-nvim
-  ];
-
-  homeModules.nvim.lua = [
-    ''
-      local blankline = require('ibl')
-      blankline.setup()
-    ''
-  ];
+  # programs.neovim.plugins = with pkgs.vimPlugins; [
+  #   indent-blankline-nvim
+  # ];
+  #
+  # homeModules.nvim.lua = [
+  #   ''
+  #     local blankline = require('ibl')
+  #     blankline.setup()
+  #   ''
+  # ];
 }
