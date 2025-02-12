48e9d54a091405bd9156a3c80fcaa9314f29516f
Simon Ashton <simonashton.dev@gmail.com>
2025-02-11 17:49:07 -0800

    Use kanshi to manage docked/undocked monitor setup

--- home_modules/desktop/sway/output.nix
+++ home_modules/desktop/sway/output.nix
@@ -1,9 +1,31 @@
-{ ... }:
+{ lib, config, ... }:
 
 {
-  wayland.windowManager.sway.config.output = {
-    HDMI-A-1 = {
-      pos = "0 0";
-    };
+  services.kanshi = lib.mkIf config.homeModules.sway.enable {
+    enable = true;
+    settings = [
+      {
+        profile.name = "undocked";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+          }
+        ];
+      }
+     
+      {
+        profile.name = "docked";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            position = "1680,0";
+          }
+          {
+            criteria = "HDMI-A-1";
+            position = "0,0";
+          }
+        ];
+      }
+    ];
   };
 }
