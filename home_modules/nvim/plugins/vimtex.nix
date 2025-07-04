871581ca342cabaad4f9775326c33837a151ce94
Simon Ashton <simonashton.dev@gmail.com>
2025-07-03 18:16:47 -0700

    Move vimtex output to build folder

--- home_modules/nvim/plugins/vimtex.nix
+++ home_modules/nvim/plugins/vimtex.nix
@@ -14,6 +14,9 @@
     homeModules.nvim.lua = [ 
       ''
         vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_compiler_latexmk = {
+          out_dir = 'build'
+        }
         vim.keymap.set('n', '<leader>tc', '<cmd>VimtexCompile<CR>', { desc = 'VimTeX | Compile', silent = true })
         vim.keymap.set('n', '<leader>tv', '<cmd>VimtexView<CR>', { desc = 'VimTeX | View', silent = true })
       ''
