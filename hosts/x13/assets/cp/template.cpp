1ff782253d01fbb542dd53d362851324d24f9178
Simon Ashton <simonashton.dev@gmail.com>
2025-07-12 18:57:26 -0700

    Revert "Change CP .cpp template"
    This reverts commit 4d18cd963e765051f4c35a5937356c87aaf67c5d.

--- hosts/x13/assets/cp/template.cpp
+++ hosts/x13/assets/cp/template.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-typedef long long ll;
+using namespace std;
+using ll = long long;
 
 void solution() {
 
